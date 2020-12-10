66aad63b640447ad7e7b1f5e5e64a0967445aa04
Jeremy Smith <smithteamone@outlook.com>
2020-12-09 18:37:31 -0600

    HashMap

---
+++ 01-java-fundamentals/07-map-of-the-hashmatique/HashMapFun.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+import java.util.Set;
+
+public class HashMapFun {
+    public static void main(String[] args){
+        HashMap<String, String> trackList = new HashMap<String, String>();
+        trackList.put("christmas tree", "oh christmas tree, how lovely are your branches");
+        trackList.put("jingle bells", "jingle bells, jingle bells, jingle all the way");
+        trackList.put("feliz navidad", "feliz navidad, prospero ano felicidad");
+        trackList.put("two pina coladas", "One for each hand");
+
+        String name = trackList.get("feliz navidad");
+
+        // get the keys by using the keySet method
+        Set<String> keys = trackList.keySet();
+        for(String key : keys) {
+            System.out.printf("track: %s Lyrics: %s%n", key, trackList.get(key));
+            
+        }
+
+    }
+}
