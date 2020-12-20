e7e0e54a564a46734441c4aa060cf6a7df3e9228
Jeremy Smith <smithteamone@outlook.com>
2020-12-20 08:08:28 -0600

    ObjectMaster2

---
+++ 02-java-oop/06-object-master-p2/Test.java
@@ -0,0 +1,11 @@
+public class Test {
+    public static void main(String [] args){
+        Ninja ninjie = new Ninja();
+        Samurai samie = new Samurai();
+        Wizard wizie = new Wizard();
+        wizie.fireball(ninjie);
+
+
+    }
+    
+}
